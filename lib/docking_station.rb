e779aa392b01f69e114c4faf64dbbc362ce48042
Anthony <a.a.donovan@outlook.com>
2020-05-13 17:51:29 +0100

    Nearly at the end of 14, error with docking station being full

--- lib/docking_station.rb
+++ lib/docking_station.rb
@@ -1,16 +1,27 @@
 require_relative 'bike'
 class DockingStation
-  attr_reader :bike
+  #attr_reader :bikes
+  def bikes
+    @bikes.last
+  end
+
+  def initialize
+    @bikes = []
+  end
 
   def release_bike
-    unless (bike.nil? == false)
-      raise "this docking station is empty"
+    if @bikes.count == 0
+      fail "this docking station is empty"
     end
     bike = Bike.new
-
   end
 
   def dock(bike)
-    @bike = bike
+    if @bikes.count >= 19
+      p "It failed"
+      fail "this docking station is full"
+    end
+    @bikes << bike
+    p "The current number of bikes is: " + @bikes.count.to_s
   end
 end
--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -3,14 +3,14 @@ require 'bike'
 
 describe DockingStation do
   describe 'release_bike' do
-    docking_station = DockingStation.new
     it 'gets a bike a expect to be working' do
       bike = Bike.new
-      docking_station.dock(bike)
-      expect(docking_station.release_bike.working?).to eq(true) 
+      subject.dock(bike)
+      expect(subject.release_bike.working?).to eq(true)
     end
+
     it 'returns an error message if docking station empty' do
-      expect { docking_station.release_bike}.to raise_error("this docking station is empty")
+      expect { subject.release_bike}.to raise_error("this docking station is empty")
     end
   end
 
@@ -18,8 +18,14 @@ describe DockingStation do
     docking_station = DockingStation.new
     it 'takes a bike and allows access to it' do
       bike = Bike.new
-      docking_station.dock(bike)
-      expect(docking_station.bike).to eq bike
+      subject.dock(bike)
+      expect(subject.bikes).to eq bike
+    end
+
+    it 'returns an error message if docking station is full' do
+      bike = Bike.new
+      20.times {subject.dock(bike)}
+      expect { subject}.to raise_error("this docking station is full")
     end
   end
 end
