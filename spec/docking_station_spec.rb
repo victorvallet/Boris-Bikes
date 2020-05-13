2d92153967379b31962ee9361e6a70ea2a93ae63
victorvallet <victorvallet8@gmail.com>
2020-05-13 15:00:09 +0100

    New boris-bikes folder due to error

--- spec/docking_station_spec.rb
+++ spec/docking_station_spec.rb
@@ -1,8 +1,13 @@
 require 'docking_station'
+require 'bike'
 
 describe DockingStation do
 
   describe 'release_bike' do
-
+    docking_station = DockingStation.new
+  it 'gets a bike a expect to be working' do
+    expect(docking_station.release_bike.working?).to eq(true)
   end
+  end
+  
 end
